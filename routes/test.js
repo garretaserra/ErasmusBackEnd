1e496584f811052fc662f7498c8465557493d98d
sergi <sergi@unievegroup.com>
2019-10-04 17:29:04 +0200

    Deleted javascript files that are autogenerated with tsc

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-let router = express.Router();
-let testScripts = require('../controllers/testScripts');
-router.get('/get', testScripts.test);
-router.post('/post', testScripts.test);
-module.exports = router;
--- routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-let router = express.Router();
-let userScripts = require('./../controllers/userScritps');
-router.post('/register', userScripts.register);
-router.post('/login', userScripts.login);
-module.exports = router;
