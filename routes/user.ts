050f931d8e3cb3fe817715a4eef2f382a783a993
sergi <sergi@unievegroup.com>
2019-10-08 17:53:04 +0200

    Added authentication functionality to the server routes

--- routes/user.ts
+++
@@ -1,9 +0,0 @@
-import express = require('express');
-let router: express.Router = express.Router();
-
-let userScripts = require('./../controllers/userScritps');
-
-router.post('/register', userScripts.register);
-router.post('/login', userScripts.login);
-
-module.exports = router;
