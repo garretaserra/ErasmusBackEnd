09f83e50bad13289926005f432061bce1bb7b0ec
sergi <sergi@unievegroup.com>
2019-12-04 11:59:19 +0100

    Fixed port for CORS url

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ else {
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
