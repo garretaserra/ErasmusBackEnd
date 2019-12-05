c16438b41d858bcea2a26acbf7c8c2b0933b9c87
sergi <sergi@unievegroup.com>
2019-12-05 10:01:48 +0100

    When token has expired session token is removed so that user can log in again

--- routes/user.ts
+++ routes/user.ts
@@ -5,8 +5,8 @@ let userScripts = require('./../controllers/userScripts');
 let auth = require('./auth');
 
 
-userRouter.post('/register', auth.optional, userScripts.register);
-userRouter.post('/login', auth.optional, userScripts.login);
+userRouter.post('/register', userScripts.register);
+userRouter.post('/login', userScripts.login);
 //Example of route that needs authentication
 userRouter.get('/user', auth.required, function (req, res){res.status(200).send({message: 'It works'})});
 
