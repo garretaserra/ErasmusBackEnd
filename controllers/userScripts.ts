a8790325befa224b98a0b711c2ae4eb184ff621d
carlogattuso <carlogc46@gmail.com>
2019-12-11 22:20:02 +0100

    Swagger updated

--- controllers/userScripts.ts
+++ controllers/userScripts.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-import { Request, Response } from 'express';
 import User from '../models/user';
 import Profile from '../models/profile';
 import AuthUser from "../models/authUser";
