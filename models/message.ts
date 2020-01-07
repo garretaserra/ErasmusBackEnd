d175104de1c3091439856898a761b6a38cc97606
Izan Pérez Cosano <izanperez98@gmail.com>
2020-01-07 16:50:29 +0100

    added message schema

---
+++ models/message.ts
@@ -0,0 +1,10 @@
+import {Schema, model} from 'mongoose';
+
+const MessageSchema: Schema = new Schema({
+    author: { type: String, required: true, unique: true },
+    destination: { type: String, required: true },
+    text: { type: String, required: true },
+    timestamp: {type: Date, required: true}
+});
+
+export default model('Message', MessageSchema);
